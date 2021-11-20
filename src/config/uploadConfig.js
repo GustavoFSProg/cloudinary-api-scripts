e1b472d43292e6b7f3ca79fa4c32eba3f0f56928
Gustavo <gustavo.prog40@gmail.com>
2021-11-20 11:38:36 -0300

    script-perfeito

--- src/config/uploadConfig.js
+++ src/config/uploadConfig.js
@@ -4,7 +4,6 @@ import path from 'path'
 export default {
   // eslint-disable-next-line new-cap
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'uploads'),
     filename: (req, file, cb) => {
       cb(null, file.originalname)
     },
